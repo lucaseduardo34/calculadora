4992a62c9fab73d88d8fa5a876db04949ebb45b7
lucaseduardo34 <iriwiuy@gmail.com>
2024-05-14 15:31:04 -0300

    calculadora

---
+++ calculadora.py
@@ -0,0 +1,25 @@
+print("1: Soma")
+print("2: Subtração")
+print("3: Multiplicação")
+
+
+
+escolha = int(input("Sua escolha: "))
+
+if escolha == 34:
+    x = float(input("Numero 1: "))
+else:
+    x = float(input("Numero 1: "))
+    n = float(input("Numero 2: "))
+
+if escolha == 1:
+    soma = x + n
+    print("O resultado é:", soma)
+    
+    if escolha == 2:
+        sub = x - n
+        print ("O resultado é:", sub)
+ 
+if escolha == 3:
+        mult = x * n
+        print ("O resultado é:", mult)
